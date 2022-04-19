d61de2a7644efe462fb513c4669c0965cb6e5931
EvertonBitencourt <everton.silvaa@gmail.com>
2022-04-19 11:56:07 -0300

    calculadora em JS

---
+++ script.js
@@ -0,0 +1,26 @@
+// tipos de funções
+
+//declarativas
+function funcao(){
+    console.log('Sou uma mensagem de uma função declarativa');
+}
+
+funcao();
+
+// expressão de funcao
+// com nomeação
+
+var funcao1 = function(){
+    console.log('Sou uma mensagem de funcao de expressão!');
+}
+
+funcao1();
+
+//arrow function
+var funcao2 = () =>{
+    console.log('Sou um arrow function');
+}
+
+funcao2();
+
+//Criando uma calculadora em JS
